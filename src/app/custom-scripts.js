44946d7c15c9803660f938a91902ba95b02545b1
Ana Rosner <ana@alumni.tufts.edu>
2017-05-03 17:52:58 -0400

    Add "slideshow" functionality to automatically cycle through mainstage sections while leaving floating panel unchanged. Hide Nav Bar. Still need to disable scrolling functionality.

--- src/app/custom-scripts.js
+++ src/app/custom-scripts.js
@@ -8,5 +8,39 @@ define(["dojo/topic"], function(topic) {
     /*
      * Custom Javascript to be executed when the application is ready goes here
      */
-  });
+	 
+	// custom slideshow
+	
+	var slideIndex = 0;
+	var x = document.getElementsByClassName("mainMediaContainer");
+
+	var i = 0;
+
+	function carousel() {
+		
+		//for testing, set to cycle through slideshow only 10x
+		if ( i<1000 ) {
+			console.log(i);
+			setTimeout(carousel, 5000);
+			i=i+1;
+		}
+		slideNext = slideIndex+1;
+		if (slideNext > x.length-1) {slideNext = 0}
+
+		app.ui.mainStage.beforeMainMediaUpdate(slideNext);
+		app.ui.mainStage.updateMainMediaWithStoryMainMedia(slideNext)
+		//x[slideIndex].className = "mainMediaContainer";
+		//x[slideNext].className = "mainMediaContainer active";
+
+		slideIndex = slideNext;
+
+	}//end carousel function
+	
+	console.log("begin slideshow");
+	carousel();
+	
+	//end slideshow custom code
+	
+	
+  }); //end topic.subscribe call
 });
